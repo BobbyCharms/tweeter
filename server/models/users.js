82089b7c032584fd52dc9c86dcfa6318eb94b196
BobbyCharms <robertcharming@Gmail.com>
2023-04-18 14:16:06 -0400

    first pass at user model

--- server/models/users.js
+++ server/models/users.js
@@ -2,21 +2,27 @@ const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
     id: {
-        type: DataTypes.INTEGER,
+        type: Integer,
         allowNull: false,
         primaryKey: true,
         autoIncrement: true,
     },
-    // preserves user entered capitalization
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!'],
+      },
     username: {
-        type: DataTypes.STRING,
+        type: String,
+        required: true,
         allowNull: false,
         validate: {
             len: [1],
         },
     },
     unique_username: {
-        type: DataTypes.STRING,
+        type: String,
         // unique users only
         unique: true,
         validate: {
@@ -24,17 +30,27 @@ const userSchema = new Schema({
         },
     },
     password: {
-        type: DataTypes.STRING,
+        type: String,
+        required: true,
         allowNull: false,
         validate: {
             // requires password to have one digit, one lowercase, one uppercase, and be at least 8 characters long
             is: /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}/,
         },
     },
+    tweeterYellow: {
+        type: Boolean,
+        allowNull: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+      },
 });
 
-const Users = model('Profile', profileSchema);
+const Users = model('User', userSchema);
 
-module.exports = Profile;
+module.exports = User;
 
 
