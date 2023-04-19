ecc3e4af80726b77443c34b2712bbedff63eed1d
Shini <halimshini@hotmail.com>
2023-04-19 14:31:22 -0400

    added fields for name email pasword on register page

--- client/src/components/pages/Register.js
+++ client/src/components/pages/Register.js
@@ -12,6 +12,48 @@ function Register() {
   };
 
   return 
-  (<></>)
+  ( <Container>
+    <Row className="justify-content-md-center">
+      <Col md={6}>
+        <h2 className="my-4 text-center">Register</h2>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="formName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter your name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit" style={{marginTop:'20px'}}>
+            Register
+          </Button>
+        </Form>
+      </Col>
+    </Row>
+  </Container>
+  )
 
 export default Register;
