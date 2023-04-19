6387e17427ebf2279407dbae7e0702f050257bde
tatys5394 <123910162+tatys5394@users.noreply.github.com>
2023-04-19 14:01:53 -0400

    adds footer code

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Footer() {
+    return(
+        <div>
+            <footer>
+                <p>With ❤️ Robert, Liza, Elias, Tatiana, David, Halim, Stevi</p>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
