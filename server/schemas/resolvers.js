37bc38234b32feabb15bb1a26730f45ee2be09fd
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-04-19 10:43:49 -0400

    committing for pull request for related material

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,7 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
 const resolvers = {
   Query: {
     users: async () => {
@@ -11,6 +15,12 @@ const resolvers = {
       ];
     },
   },
+  Mutation: {
+    createUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user)
+    }
+  }
 };
 
 module.exports = resolvers;
