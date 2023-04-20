33738d0ae8f413f2a7fe6d985c31bbe88d9761c6
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-04-20 13:15:25 -0400

    adds initial functionality to login page

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,9 +1,45 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import React, { useState } from 'react';
 import loginImg from '../../assets/images/login.svg';
+import { useMutation } from '@apollo/client';
+import { LOGIN } from '../../utils/mutations';
+
 import Auth from '../../utils/auth';
 
-function Login() {
+// function Login() {
+const Login = (props) => {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error, data }] = useMutation(LOGIN);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setFormState({
+      email: '',
+      password: '',
+    });
+  };
+
   return (
     <div className="d-flex justify-content-around">
       <div className="d-flex justify-content-start">
@@ -45,6 +81,6 @@ function Login() {
       </div>
     </div>
   );
-}
+};
 
 export default Login;
