c84a3feda4d09223b54491d604304113d506b051
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-04-20 13:21:54 -0400

    updated a bunch of .js to .jsx

--- client/src/components/homepage/Homepage.jsx
+++ client/src/components/homepage/Homepage.jsx
@@ -1,19 +1,29 @@
-import Twit from "../Twit"
-import twits from "../../utils/dummyTwitData"
+import Twit from '../Twit';
+import twits from '../../utils/dummyTwitData';
+import Login from '../pages/Login';
 
 const newTwit = () => {
-    //create a new twit
-}
+  //create a new twit
+};
 
-function Homepage() {return(
+function Homepage() {
+  return (
     <div>
-        <div>
-            <a href="#"><p>Homepage</p></a>
-            <a href="#"><p>My Tweeter</p></a>
-        </div>
-        {twits.map(item => <Twit username={item.username} twit={item.twit}/>)}
-        <button onClick={newTwit}>+</button>
+      <Login />
+      <div>
+        <a href="#">
+          <p>Homepage</p>
+        </a>
+        <a href="#">
+          <p>My Tweeter</p>
+        </a>
+      </div>
+      {twits.map((item) => (
+        <Twit username={item.username} twit={item.twit} />
+      ))}
+      <button onClick={newTwit}>+</button>
     </div>
-)};
+  );
+}
 
 export default Homepage;
