2d6e3f775bc1c74a171ccaa048fedce3a14cc586
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-04-20 12:21:28 -0400

    adds structure for queries on front end

---
+++ client/src/utils/queries.js
@@ -0,0 +1,16 @@
+import { gql } from `@apollo/client;`
+
+export const QUERY_USERS = gql`
+`;
+
+export const QUERY_SINGLE_USER = gql`
+`;
+
+export const QUERY_TWITS = gql`
+`;
+
+export const QUERY_USER_TWITS = gql`
+`;
+
+export const QUERY_SINGLE_TWIT = gql`
+`;
