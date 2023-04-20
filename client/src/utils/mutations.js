bd32dc7684360bee305bdcf9d539e86a0e244752
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-04-20 12:11:35 -0400

    sketches out mutation queries

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,29 @@
+import { gql } from `@apollo/client;`
+
+export const CREATE_USER = gql`
+`;
+
+export const LOGIN = gql`
+`;
+
+export const DELETE_USER = gql`
+`;
+
+export const ADD_TWIT = gql`
+`;
+
+export const EDIT_TWIT = gql`
+`;
+
+export const DELETE_TWIT = gql`
+`;
+
+export const ADD_COMMENT = gql`
+`;
+
+export const EDIT_COMMENT = gql`
+`;
+
+export const DELETE_COMMENT = gql`
+`;
+
