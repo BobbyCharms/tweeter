06d19ae635e4ff05cbc634190a055ef412672088
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-04-21 13:39:16 -0400

    conditional navbar to test loggedUser

--- client/src/components/homepage/Homepage.jsx
+++ client/src/components/homepage/Homepage.jsx
@@ -6,49 +6,14 @@ import { useQuery } from '@apollo/client';
 import { QUERY_TWITS } from '../../utils/queries';
 
 const Homepage = () => {
-    const { loading, data } = useQuery(QUERY_TWITS);
-    const twits = data?.twits || [];
+  const { loading, data } = useQuery(QUERY_TWITS);
+  const twits = data?.twits || [];
 
-
-    return (
-        <>
-        {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <ManyTwits
-              twits={twits}
-            />
-          )}
-        </>
-    )
-}
+  return <>{loading ? <div>Loading...</div> : <ManyTwits twits={twits} />}</>;
+};
 
 const newTwit = () => {
   //create a new twit
 };
-
-// function Homepage() {
-//   return (
-//     <div>
-//       <div>
-//         <Link to="/">
-//           <p>Homepage</p>
-//         </Link>
-//         {loggedIn() ? (
-//           <button type="button" onClick={() => logout()}>
-//             Log out
-//           </button>
-//         ) : (
-//           <Link to="/login">
-//             <p>Login</p>
-//           </Link>
-//           // Link to mytweeter
-//         )}
-//       </div>
-//       <ManyTwits />
-//       <button onClick={newTwit}>+</button>
-//     </div>
-//   );
-// }
 
 export default Homepage;
