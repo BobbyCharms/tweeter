e22ffcb5eed16626307adf3755aa8ab478300496
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-04-21 13:31:06 -0400

    prettier and removes unused dependecies

--- client/src/components/pages/LoggedUserTwit.jsx
+++ client/src/components/pages/LoggedUserTwit.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
-import { Link } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
 import { QUERY_USER_TWITS } from '../../utils/queries';
 import ManyTwits from '../../twit/manyTwits';
 import { getToken } from '../../utils/auth';
