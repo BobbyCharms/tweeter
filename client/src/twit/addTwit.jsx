6d2214473731bee2f77441ec10dcdc0487fe645f
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-04-21 17:41:46 -0400

    continued trying

--- client/src/twit/addTwit.jsx
+++ client/src/twit/addTwit.jsx
@@ -1,9 +1,32 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_TWIT } from '../utils/mutations';
+import { QUERY_TWITS, QUERY_SINGLE_USER } from '../utils/queries';
 
 // import ManyTwits from '../../twit/manyTwits';
 
-const AddTwit = ({ addTwitMutation, twitText, setTwitText }) => {
+const AddTwit = () => {
   const [showTextArea, setShowTextArea] = useState(false);
+  const [addTwitMutation, { error }] = useMutation(ADD_TWIT, {
+    // The update method allows us to access and update the local cache
+    update(cache, { data: { addTwit } }) {
+      try {
+        // First we retrieve existing profile data that is stored in the cache under the `QUERY_PROFILES` query
+        // Could potentially not exist yet, so wrap in a try/catch
+        const { twits } = cache.readQuery({ query: QUERY_TWITS });
+
+        // Then we update the cache by combining existing profile data with the newly created data returned from the mutation
+        cache.writeQuery({
+          query: QUERY_TWITS,
+          // If we want new data to show up before or after existing data, adjust the order of this array
+          data: { twits: [addTwit, ...twits] },
+        });
+      } catch (e) {
+        console.error(e);
+      }
+    },
+  });
+  const [twitText, setTwitText] = useState('');
 
   const handleShowTextAreaClick = () => {
     setShowTextArea(!showTextArea);
@@ -20,6 +43,8 @@ const AddTwit = ({ addTwitMutation, twitText, setTwitText }) => {
 
   // }
 
+  console.log('homepage addtwit error', error);
+
   return (
     <>
       <button onClick={handleShowTextAreaClick}>
@@ -28,7 +53,11 @@ const AddTwit = ({ addTwitMutation, twitText, setTwitText }) => {
       {showTextArea && (
         <div className="input-group">
           <span className="input-group-text">With textarea</span>
-          <textarea className="form-control" aria-label="With textarea" onChange={setTwitText}/>
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            onChange={(event) => setTwitText(event.target.value)}
+          />
           <button onClick={handleTwitSubmit}>TWIT</button>
         </div>
       )}
