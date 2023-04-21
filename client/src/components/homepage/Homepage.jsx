75a74f889799c0bfda0cf81a982e81462f6a99d5
Elias Spector-Zabusky <123032126+ColdWeatherBoyy@users.noreply.github.com>
2023-04-21 15:48:18 -0400

    Update Homepage.jsx

--- client/src/components/homepage/Homepage.jsx
+++ client/src/components/homepage/Homepage.jsx
@@ -16,35 +16,35 @@ const Homepage = () => {
 };
 
 
-const newTwit = () => {
-  //create a new twit
-};
+// const newTwit = () => {
+//   //create a new twit
+// };
 
-function Homepage() {
-  return (
-    <div className={styles.container}>
-      <div className={styles.header}>
-        <Link to="/" className={styles.link}>
-          <p>Homepage</p>
-        </Link>
-        {loggedIn() ? (
-          <button type="button" onClick={() => logout()}>
-            Log out
-          </button>
-        ) : (
-          <Link to="/login" className={styles.link}>
-            <p>Login</p>
-          </Link>
-        )}
-      </div>
-      <div className={styles.twitsContainer}>
-        <ManyTwits />
-        <button onClick={newTwit} className={styles.newTwitButton}>
-          +
-        </button>
-      </div>
-    </div>
-  );
-}
+// function Homepage() {
+//   return (
+//     <div className={styles.container}>
+//       <div className={styles.header}>
+//         <Link to="/" className={styles.link}>
+//           <p>Homepage</p>
+//         </Link>
+//         {loggedIn() ? (
+//           <button type="button" onClick={() => logout()}>
+//             Log out
+//           </button>
+//         ) : (
+//           <Link to="/login" className={styles.link}>
+//             <p>Login</p>
+//           </Link>
+//         )}
+//       </div>
+//       <div className={styles.twitsContainer}>
+//         <ManyTwits />
+//         <button onClick={newTwit} className={styles.newTwitButton}>
+//           +
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
 
 export default Homepage;
