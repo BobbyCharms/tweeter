47cd05423c24393b041b1753316783b12510c1a9
tatys5394 <123910162+tatys5394@users.noreply.github.com>
2023-04-21 12:09:55 -0400

    styles twit cards

--- client/src/twit/Twit.jsx
+++ client/src/twit/Twit.jsx
@@ -1,25 +1,41 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import "./Twit.css";
-import { HandThumbsUp, HandThumbsDown, Chat  } from 'react-bootstrap-icons';
+import Container from 'react-bootstrap/Container';
+import './Twit.css';
+import { HandThumbsUp, HandThumbsDown, Chat } from 'react-bootstrap-icons';
 
-function Twit(props) {return(
+const containerStyles = {
+    margin: '30px'
+}
+
+const cardStyles = {
+  width: '36rem',
+};
+
+function Twit(props) {
+  return (
     <div className="twit">
-        <Card>
-            <Card.Header as="h5"><b>@{props.username}:</b></Card.Header>
-            <Card.Body>
-                <Card.Title>{props.twit}</Card.Title>
-             <Card.Text>
-                {props.createdOn}
-                </Card.Text>
-                <Chat />
-            </Card.Body>
-            </Card>
-        <div>
-            <HandThumbsUp />
-            <HandThumbsDown />
-        </div>
+        <Container className="d-flex justify-content-center mb-5">
+      <Card style={cardStyles}>
+        <Card.Header as="h5">
+          <b>@{props.username}:</b>
+        </Card.Header>
+        <Card.Body>
+          <Card.Title>{props.twit}</Card.Title>
+          <Card.Text>{props.createdOn}</Card.Text>
+          <Container className="d-flex justify-content-start">
+            <Chat className="mx-3"/>
+            <HandThumbsUp className="mx-3"/>
+            <HandThumbsDown className="mx-3"/>
+          </Container>
+        </Card.Body>
+      </Card>
+      <div>
+      </div>
+      
+    </ Container>
     </div>
-)};
+  );
+}
 
 export default Twit;
