f979f5b00bf6cf780db1b9eb7de8ddc572ae9cd6
tatys5394 <123910162+tatys5394@users.noreply.github.com>
2023-04-21 11:32:29 -0400

    attempt 2

--- client/src/twit/Twit.jsx
+++ client/src/twit/Twit.jsx
@@ -1,13 +1,23 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import "./Twit.css";
 import { HandThumbsUp, HandThumbsDown, Chat  } from 'react-bootstrap-icons';
 
 function Twit(props) {return(
     <div className="twit">
-        <p><b>@{props.username}:</b> {props.twit}</p>
+        <Card>
+            <Card.Header as="h5"><b>@{props.username}:</b></Card.Header>
+            <Card.Body>
+                <Card.Title>{props.twit}</Card.Title>
+             <Card.Text>
+                {props.createdOn}
+                </Card.Text>
+                <Chat />
+            </Card.Body>
+            </Card>
         <div>
             <HandThumbsUp />
             <HandThumbsDown />
-            <Chat />
         </div>
     </div>
 )};
