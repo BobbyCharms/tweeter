644aa11f92c7879dba8497d28adef65e2492c7fd
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-04-23 16:23:39 -0400

    adds comment delete

--- client/src/components/comments/comment.jsx
+++ client/src/components/comments/comment.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import './comments.css';
 import { Link } from 'react-router-dom';
 import { HandThumbsUp, HandThumbsDown, Chat } from 'react-bootstrap-icons';
+import { DELETE_COMMENT } from '../../utils/mutations';
+import { getUser, loggedIn } from '../../utils/auth';
+import { useMutation } from '@apollo/client';
 
 const containerStyles = {
   height: 'auto',
@@ -12,18 +15,49 @@ const cardStyles = {
   width: '36rem',
 };
 
-function Comment(props) {
-  console.log(props);
+const Comment = (props) => {
+  const [userId, setUserId] = useState('');
+  const [isDeleted, setIsDeleted] = useState(false);
+
+  if (loggedIn()) {
+    useEffect(() => {
+      setUserId(getUser().data._id);
+    });
+  }
+
+  const [deleteCommentMutation, { error, data }] = useMutation(DELETE_COMMENT);
+
+  const handleDelete = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await deleteCommentMutation({
+        variables: { commentId: props.id },
+      });
+      data ? setIsDeleted(true) : setIsDeleted(false);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  if (isDeleted) {
+    return null; // If the Twit has been deleted, render nothing
+  }
+
   return (
     <Container
       className="d-flex justify-content-center mb-5"
       style={containerStyles}
     >
       <Card style={cardStyles}>
-        <Card.Header as="h5">
+        <Card.Header as="h5" className="d-flex justify-content-between">
           <Link to={`/${props.userId}`}>
             <b>@{props.username}</b>
           </Link>
+          {props.userId === userId && loggedIn() ? (
+            <button onClick={handleDelete}>x</button>
+          ) : (
+            <></>
+          )}
         </Card.Header>
         <Card.Body>
           <Card.Title>{props.comment}</Card.Title>
@@ -40,6 +74,6 @@ function Comment(props) {
       </Card>
     </Container>
   );
-}
+};
 
 export default Comment;
