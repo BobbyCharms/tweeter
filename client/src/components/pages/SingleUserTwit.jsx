3b60e36dd53b32ff92e41d58beb57e3093429f31
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-04-23 12:14:30 -0400

    sets up rerouting for user Id matching loggedin Id

--- client/src/components/pages/SingleUserTwit.jsx
+++ client/src/components/pages/SingleUserTwit.jsx
@@ -3,9 +3,14 @@ import { useQuery } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 import { QUERY_USER_TWITS } from '../../utils/queries';
 import ManyTwits from '../../twit/manyTwits';
+import { getUser } from '../../utils/auth';
 
 const SingleUserTwit = () => {
   const { id } = useParams();
+  if (id === getUser().data._id) {
+    // reload the page at /user/:id
+    window.location.assign('/user/' + id);
+  }
   console.log(useParams());
   const { loading, error, data } = useQuery(QUERY_USER_TWITS, {
     variables: { userId: id },
